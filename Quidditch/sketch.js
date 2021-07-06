b4c0fbd0bf0966a653708dd96632105468015616
anweshasingh <72261344+anweshasingh@users.noreply.github.com>
2021-07-06 19:54:42 +0530

    Update sketch.js

--- Quidditch/sketch.js
+++ Quidditch/sketch.js
@@ -16,6 +16,7 @@ function preload() {
   pitchImg = loadImage("background.jpg");
   bludgerImg = loadImage("bludger.png");
 }
+
 function setup() {
   createCanvas(1200,800);
 
@@ -26,58 +27,66 @@ function setup() {
   player = createSprite(70, 200, 50, 50);
   player.addImage(playerImg);
  
+  
+  quaffleGroup = new Group();
+  gsGroup = new Group();
+  bludgerGroup = new Group();
  
 }
 
 function draw() {
-background("black")
-text("SCORE: ", score, 50,300);
-  pitch.velocityX =  -3;
-  if (pitch.x < 0){
-    pitch.x = pitch.width/2;
-  }
+background("white")
+fontSize = 50
+fill("black")
+text("SCORE: ", score, 100,300);
+if(gameState === PLAY){
+  pitch.velocityX = -3;
 
+  if (pitch.x < 500){
+    pitch.x = 600;
+  }
   if(keyDown(UP_ARROW)){
     player.y = player.y-10
   }
   if(keyDown(DOWN_ARROW)){
     player.y = player.y+10
   }
-  if(keyDown(RIGHT_ARROW)){
-    player.x = player.x+10
+  
+   if(quaffleGroup.isTouching(player)){
+    quaffleGroup.destroyEach();
+    score = score-20
   }
-  if(keyDown(LEFT_ARROW)){
-    player.x = player.x-10
+  if (bludgerGroup.isTouching(player)) {
+    bludgerGroup.destroyEach();
+    score = score+20
   }
+  if(gsGroup.isTouching(player)){
+    score = score+150
+    gameState = END
+    fontSize = 20
+    text("YOU WIN!!", 600,400);
+  }
+}
+else if(gameState === END) {
+  pitch.velocityX = 0
+  quaffleGroup.setVelocityXEach(0);
+  gsGroup.setVelocityXEach(0);
+  bludgerGroup.setVelocityXEach(0);
 
-  //if(quaffleGroup.isTouching(player)){
-    //score = score-20
-  //}
-  //if (bludgerGroup.isTouching(player)) {
-    //score = score+20
-  //}
-
-  //if(gsGroup.isTouching(player)){
-    //score = score+150
-    //fontSize = 20
-    //text("YOU WIN!!", 600,400);
-  //}
-
-
-
+  quaffleGroup.setLifetimeEach(-1);
+  gsGroup.setLifetimeEach(-1);
+  bludgerGroup.setLifetimeEach(-1);
+}
 
   spwanQ();
-  quaffleGroup = new Group();
   spwanGS();
-  gsGroup = new Group();
   spwanBludger();
-  bludgerGroup = new Group();
   drawSprites();
 }
 
 function spwanQ() {
-  if(frameCount % 70 ===0){
-    quaffle = createSprite(Math.round(random(100,1800)),Math.round(random(200,800)),40,10)
+  if(frameCount % 80 ===0){
+    quaffle = createSprite(Math.round(random(10,1100)),Math.round(random(20,700)),40,10)
     quaffle.addImage(quaffleImg)
     quaffle.velocityX = -5
     quaffle.scale = 0.1
@@ -86,8 +95,8 @@ function spwanQ() {
 }
 
 function spwanGS() {
-  if(frameCount % 90 ===0){
-    gs = createSprite(Math.round(random(100,1800)),Math.round(random(200,800)),40,10)
+  if(frameCount % 100 ===0){
+    gs = createSprite(Math.round(random(10,1100)),Math.round(random(20,700)),40,10)
     gs.addImage(gsImg)
     gs.velocityX = -10
     gs.scale = 0.1
@@ -96,8 +105,8 @@ function spwanGS() {
 }
 
 function spwanBludger() {
-  if(frameCount % 50 ===0){
-    bludger = createSprite(Math.round(random(100,1800)),Math.round(random(200,800)),40,10)
+  if(frameCount % 80 ===0){
+    bludger = createSprite(Math.round(random(10,1100)),Math.round(random(20,700)),40,10)
     bludger.addImage(bludgerImg)
     bludger.velocityX = -5
     bludger.scale = 0.5
